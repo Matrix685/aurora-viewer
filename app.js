5331d22ee1295f0a654da580d458c3263e118f0a
matrix685 <oflynnc48len@gmail.com>
2025-09-14 14:00:07 +0100

    auto-download using time, styles and images

--- app.js
+++ app.js
@@ -3,53 +3,70 @@ const https = require("https");
 const Readable = require("stream").Readable;
 const shell = require("shelljs");
 
-const url = "https://comicaurora.com/";
-let imageUrl;
+getComic(); // for testing
 
-const auroraHTML = fs.createWriteStream("index.html");
+function getComic() {
+    const url = "https://comicaurora.com/";
+    let imageUrl;
 
-const request = https.request(url, (response) => {
-    let data = "";
+    const auroraHTML = fs.createWriteStream("index.html");
 
-    response.on("data", (chunk) => {
-        data += chunk;
-    });
+    const request = https.request(url, (response) => {
+        let data = "";
 
-    response.on("end", function () {
-        const lines = data.split("\n");
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
 
-        for (const line of lines) {
-            if (line.includes('<div id="mgsisk_webcomic_collection_widget_webcomicmedia-5"')) {
-                line.split(" ").forEach((imageLink) => {
-                    if (imageLink.includes('src="https://comicaurora.com/wp-content/uploads/')) {
-                        imageUrl = imageLink.substring(5, imageLink.length - 1);
+        response.on("end", function () {
+            const lines = data.split("\n");
 
-                        // console.log(imageUrl);
-                    }
-                });
-                // console.log(line);
-                break;
+            for (const line of lines) {
+                if (line.includes('<div id="mgsisk_webcomic_collection_widget_webcomicmedia-5"')) {
+                    line.split(" ").forEach((imageLink) => {
+                        if (imageLink.includes('src="https://comicaurora.com/wp-content/uploads/')) {
+                            imageUrl = imageLink.substring(5, imageLink.length - 1);
+
+                            console.log(imageUrl);
+                        }
+                    });
+                    // console.log(line);
+                    break;
+                }
             }
-        }
 
-        downloadImage(imageUrl);
+            downloadImage(imageUrl);
 
-        let replacedData = data.replaceAll(imageUrl, "images/current-page.png");
-        let s = new Readable();
+            let replacedData = data
+                .replaceAll("https://comicaurora.com/wp-content/uploads/2019/04/logo-progress.png", "images/logo-progress.png") // aurora logo
+                .replaceAll("https://comicaurora.com/wp-content/uploads/2019/12/logo-BIG.png", "images/logo-BIG.png") // aurora logo v2
+                .replaceAll("https://comicaurora.com/wp-content/uploads/2019/04/bg-aurora.png", "images/bg-aurora.png") // aurora background
+                .replaceAll("https://comicaurora.com/wp-content/uploads/2019/04/cropped-icon-32x32.png", "images/cropped-icon-32x32.png") // some icon idk
+                .replaceAll("https://comicaurora.com/wp-content/uploads/2019/04/header-wrap-small-alt.png", "images/header-wrap-small-alt.png") // vines in header
+                .replaceAll("https://comicaurora.com/wp-content/uploads/2019/04/arrow-blue-e1571699055192.png", "images/arrow-blue-e1571699055192.png") // double left arrow
+                .replaceAll("https://comicaurora.com/wp-content/uploads/2019/04/arrow--e1571699115274.png", "images/arrow--e1571699115274.png") // single left arrow
+                .replaceAll("https://comicaurora.com/wp-content/uploads/2019/04/random-e1571699137392.png", "images/random-e1571699137392.png") // random page button
+                .replaceAll("https://comicaurora.com/wp-content/uploads/2019/04/arrow-1-1-e1571699098139.png", "images/arrow-1-1-e1571699098139.png") // single right arrow
+                .replaceAll("https://comicaurora.com/wp-content/uploads/2019/04/arrow-1-2-e1571699075864.png", "images/arrow-1-2-e1571699075864.png") // double right arrow
+                .replaceAll("<head>", `<head>\n\t<link rel="stylesheet" href="styles/style.css" />`) // styles
+                .replaceAll(imageUrl, "images/current-page.png"); // comic page
 
-        s.push(replacedData);
-        s.push(null);
+            let s = new Readable();
 
-        s.pipe(auroraHTML);
-        console.log("HTML successful");
+            s.push(replacedData);
+            s.push(null);
+
+            s.pipe(auroraHTML);
+            console.log("HTML successful");
+        });
     });
-});
 
-request.on("error", (error) => {
-    console.log("An error", error);
-});
+    request.on("error", (error) => {
+        console.log("An error", error);
+    });
 
-request.end();
+    request.end();
+}
 
 function downloadImage(imageUrl) {
     const imageName = "images/current-page.png";
@@ -78,3 +95,18 @@ function commit() {
 
     console.log("all done!");
 }
+
+console.log("Waiting for new comic");
+
+let checkTime = setInterval(() => {
+    // console.log("you do work right?");
+    const time = new Date();
+
+    // console.log(time.getUTCHours().toString() + ":" + time.getUTCMinutes());
+
+    if (time.getHours() == 9 && time.getMinutes() > 15) {
+        console.log("ITS OUUUUTT!!");
+        getComic();
+        clearInterval(checkTime);
+    }
+}, 60000);
